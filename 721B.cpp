36a5b7959c3cbe174ce62a9915dcf5ecde4dfcd8
Thiago Ribeiro <thiago@toribeiro.com>
2017-06-24 23:06:05 -0400

    721B

--- 721B.cpp
+++ 721B.cpp
@@ -6,4 +6,51 @@
 //  Copyright © 2017 Thiago Ribeiro. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    int c = 0, k = 0, el = 0, dl = 0, bc = 0, w = 0, w_c = 0;
+    string p, ci;
+    vector<string> v;
+    
+    cin >> c >> k;
+    
+    int cc = c;
+    
+    while (c--) {
+        cin >> ci;
+        v.emplace_back(ci);
+    }
+    
+    cin >> p;
+    
+    for (string str : v) {
+        str.length() == p.length() ? el++ : dl++;
+    }
+    
+    el == cc ? bc = 1 : bc = dl;
+    
+    for (string str : v) {
+        if (str != p && w >= k) {
+            w_c += 5;
+            w = 0;
+        }
+        if (str != p) {
+            w++;
+            if (w >= k) {
+                w_c += 5;
+                w = 0;
+            }
+        }
+        
+    }
+    
+    cout << bc << " " << w_c+el << endl;
+    
+    return 0;
+}
+
